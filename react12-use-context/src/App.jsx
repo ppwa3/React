92d90c91095a28b1ae90295feb7f8d4a4088cbc4
ppwa3 <ppwa3@naver.com>
2025-05-22 18:30:28 +0900

    5/22

---
+++ react12-use-context/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+//컨텍스트 임포트
+import { ThemeContext } from "./context/ThemeContext";
+import Page from "./components/Page";
+// import { SimpleContext } from "./context/SimpleContext";
+//모듈화 된 컴포넌트 임포트
+
+function App() {
+  //테마변경을 위한 스테이트
+  //다크모드 변경을 위한 스테이트
+  const [isDark, setIsDark] = useState(false);
+
+  /** 데이터 공유를 위한 프로바이더는 2개 이상 겹쳐서 래핑할 수 있다. */
+  return (<>
+     {/* SimpleContext를 주석 처리하면 모듈에서 초기화 한 값이 출력되고,
+    활성화하면 value 속성으로 부여한 값이 출력된다. 즉 프로바이더로 랩핑하여
+    value로 적용한 값이 우선순위가 높다.  */}
+    {/* <SimpleContext.provider value={'Welcome 헝딜동'}> */}
+    <ThemeContext.Provider value={{isDark, setIsDark}}>
+    <div className="App">
+      <Page></Page>
+    </div>
+    </ThemeContext.Provider>
+    {/* </SimpleContext.provider> */}
+  </>);
+}
+
+export default App;
+
